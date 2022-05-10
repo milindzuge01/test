59226abb30f8c78eadb2e38183e18c4a37cfab92
Milind <milindzuge01@gmail.com>
2022-05-10 18:22:15 +0530

    add thirsty

--- hungry1.py
+++ hungry1.py
@@ -4,7 +4,10 @@ if hungry == 'yes':
 elif hungry == 's':
     print('eat fruits')
 else:
-    print('do your homework okk')
+    thirsty = input("Are you thirsty: ")
+    if thirsty == 'yes':
+        print('drink water')
+
 print('-'*20)
 print("Happy Programming")
 print('-'*20)
